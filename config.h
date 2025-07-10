650639a8c3a36868ca283ba8c5de43cb4cfc1338
rainvi <nick.schmidt1711@gmail.com_Laptop>
2025-07-10 19:49:41 +0200

    update

--- config.h
+++ config.h
@@ -8,24 +8,22 @@
 #define browser "firefox"
 #define terminal "st"
 
-/* --- Fonts --- */
-#define font "SauceCodePro Nerd Font"
-#define fontsize 15
-
 /* --- Look --- */
 #define gaps 13
 
-#define barheight 20
+#define barheight 25
 #define baropacity 0.85
 
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int startwithgaps      = 1;        /* gaps between windows */
 static const unsigned int gappx     = gaps;        /* gaps between windows */
-static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "NerdFont:size=10";
 
+/* Font "monospace:size=14", NotoColorEmoji:pixelsize=14:antialias=true:autohint=true*/
+static const char *fonts[] = { "monospace:size=16",  "NotoColorEmoji:pixelsize=16:antialias=true:autohint=true" };
 
 /* Fallback Colorscheme */
 static char normbgcolor[]     = "#121210";  // darker background
@@ -45,10 +43,6 @@ static const char *colors[][3]      = {
 	//[SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder  },
 };
 
-/* Font (reading from 'font' and 'fontsize') */
-#define FONTSTR(font, size) font ":size=" #size 
-#define FONT(font, size) FONTSTR(font, size)
-static const char *fonts[] = { FONT(font, fontsize) }; // This is also used for dmenu
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
 
@@ -88,7 +82,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() , */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normfgcolor, "-nf", normbgcolor, "-sb", normbgcolor, "-sf", normbordercolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normfgcolor, "-nf", normbgcolor, "-sb", normfgcolor, "-sf", normbordercolor, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
 static const Key keys[] = {
